27ea8f1818889f0104a07ad5d5d862567f6bc49b
kirank <kirankumaran_in@yahoo.com>
2015-08-04 10:56:54 +0530

    Base Framework is ready and created testcases according to the Excel sheet, Code refactoring is required later

--- main.py
+++ main.py
@@ -8,7 +8,66 @@ from nose.plugins.attrib import attr
 class MainDriverScript(BaseTestCase):
 
     @attr(priority="high")
-    def test_01_Filter_By_Place(self):
+    def test_AS_01_To_Verify_Delete_When_No_Assets_Are_Available(self):
+        selectAction_dropdown = self.driver.find_element_by_xpath(".//*[@id='asset_actions_dropdown']/button[2]")
+        selectAction_dropdown.click()
+        sleep(2)
+        selectAction_dropdown_delete = self.driver.find_element_by_xpath(".//*[@id='asset_actions_dropdown']/ul/li/a")
+        selectAction_dropdown_delete.click()
+        print("Nothing to delete")
+
+    @attr(priority="high")
+    def test_AS_02_To_Verify_Delete_Deselect_All_Assets(self):
+        assets_checkbox = self.driver.find_elements_by_xpath(".//*[@id='assetstable']/tbody/tr/td[1]/label/span/span[2]")
+        for asset_checkbox in assets_checkbox:
+            if asset_checkbox.is_selected():
+                asset_checkbox.click()
+
+        selectAction_dropdown = self.driver.find_element_by_xpath(".//*[@id='asset_actions_dropdown']/button[2]")
+        selectAction_dropdown.click()
+        sleep(2)
+        selectAction_dropdown_delete = self.driver.find_element_by_xpath(".//*[@id='asset_actions_dropdown']/ul/li/a")
+        selectAction_dropdown_delete.click()
+        print("Nothing to delete")
+
+    @attr(priority="high")
+    def test_AS_03_To_Verify_Delete_Asset_Should_Be_Deleted(self):
+        asset_checkbox = self.driver.find_element_by_xpath(".//*[@id='assetstable']/tbody/tr[1]/td[1]/label/span/span[2]")
+        asset_checkbox.click()
+
+        selectAction_dropdown = self.driver.find_element_by_xpath(".//*[@id='asset_actions_dropdown']/button[2]")
+        selectAction_dropdown.click()
+        sleep(2)
+        selectAction_dropdown_delete = self.driver.find_element_by_xpath(".//*[@id='asset_actions_dropdown']/ul/li/a")
+        selectAction_dropdown_delete.click()
+
+
+        delete_button = self.driver.find_element_by_xpath(".//*[@id='delete_asset_modal']/div/div/div[3]/button[2]")
+        delete_button.click()
+
+
+        print("First record deleted successfully.")
+
+    @attr(priority="high")
+    def test_AS_04_To_Verify_Delete_Asset_Cancel(self):
+        asset_checkbox = self.driver.find_element_by_xpath(".//*[@id='assetstable']/tbody/tr[1]/td[1]/label/span/span[2]")
+        asset_checkbox.click()
+
+        selectAction_dropdown = self.driver.find_element_by_xpath(".//*[@id='asset_actions_dropdown']/button[2]")
+        selectAction_dropdown.click()
+        sleep(2)
+        selectAction_dropdown_delete = self.driver.find_element_by_xpath(".//*[@id='asset_actions_dropdown']/ul/li/a")
+        selectAction_dropdown_delete.click()
+
+
+        delete_button = self.driver.find_element_by_xpath(".//*[@id='delete_asset_modal']/div/div/div[3]/button[1]")
+        delete_button.click()
+
+        print("First record cancelled successfully.")
+
+
+    @attr(priority="high")
+    def test_AS_06_To_Verify_The_Filter_Function_Filter_By_Place(self):
         sleep(10)
         self.driver.find_element_by_xpath("//*[@id='span_filters']/div/div/button[2]").click()
         sleep(10)
@@ -23,7 +82,7 @@ class MainDriverScript(BaseTestCase):
              self.assertEqual (i.text, "Place")
 
     @attr(priority="high")
-    def test_02_Filter_By_School(self):
+    def test_AS_07_To_Verify_The_Filter_Function_Filter_By_School(self):
         self.driver.find_element_by_xpath("//span[@id='span_filters']/div/div/button[2]").click()
         self.driver.find_element_by_link_text("School").click()
         sleep(10)
@@ -36,7 +95,18 @@ class MainDriverScript(BaseTestCase):
              self.assertEqual (i.text, "School")
 
     @attr(priority="high")
-    def test_03_Search_By_Name(self):
+    def test_AS_11_To_Verify_The_Reset_Filter_Function(self):
+        #self.driver.find_element_by_xpath("//span[@id='span_filters']/div/div/button[2]").click()
+        #self.driver.find_element_by_link_text("School").click()
+        #sleep(10)
+        expectedAfterResetFilter = self.driver.find_element_by_xpath("//span[@id='span_filters']/div/div/button[1]").text
+        resetFilter = self.driver.find_element_by_xpath(".//*[@id='span_filters']/button")
+        resetFilter.click()
+        self.assertEqual("Asset Type",expectedAfterResetFilter)
+
+
+    @attr(priority="high")
+    def test_AS_12_To_Verify_The_Search_For_Asset_Function_Search_By_Name(self):
         searchAsset_textbox = self.driver.find_element_by_id("txt_search_assets")
         searchAsset_textbox.send_keys("k")
         sleep(10)
