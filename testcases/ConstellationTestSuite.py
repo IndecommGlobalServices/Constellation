9380c4872214a64a4c2bb2535bd76f0427ec74ad
Deepa Sivadas <deepa.sivadas@indecomm.net>
2015-12-04 12:33:18 +0530

    Bug Fixes

---
+++ testcases/ConstellationTestSuite.py
@@ -0,0 +1,15 @@
+import nose,os
+from datetime import date, timedelta, datetime
+
+filename = "testresult-constellationtest_" + str(datetime.today().strftime('%Y%m%d')) +"_"+\
+                    str(datetime.today().time().strftime('%H%M'))+".xml"
+cwd = os.getcwd()
+os.chdir('..')
+filepath = os.path.join(os.getcwd(), filename)
+os.chdir(cwd)
+nose.run(argv=["", "assettest", "--verbosity=3","--with-xunit",
+               "--xunit-file="+filepath+"", "--nologcapture","-s", "--nocapture"])
+
+
+
+
