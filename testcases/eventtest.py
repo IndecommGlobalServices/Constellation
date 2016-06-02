7fc36596fbbb4005afeecfb7dd5a97d2d68bff4a
kiran <kiran.k@indecomm.net>
2016-06-02 14:26:21 +0530

    added 1 testcase for events - edit - details panel - cancel

--- testcases/eventtest.py
+++ testcases/eventtest.py
@@ -266,3 +266,44 @@ class EventpageTest(BaseTestCase):
         self.eventpage.get_event_detail_edit_save_button.click()  # click on Save
         self.assertTrue(self.eventpage.event_type_Saved_label.is_displayed(),
                         self.config.get(self.section, 'MESSAGE_SAVED_TEXT_NOT_DISPLAYED'))
+
+    @attr(priority="high")
+    #@SkipTest
+    def test_EV_011(self):
+        """
+        Test : test_EV_011
+        Description : To verify cancel button functionality of the Detail section.
+        Revision:
+        :return: None
+        """
+        self.eventpage.select_event_type(self.eventpage.event_name)
+        WebDriverWait(self.driver, 20).until(EC.text_to_be_present_in_element(
+            (By.XPATH, self.eventpage._event_details_edit_widget_locator), r"Details"))
+        self.eventpage.get_event_detail_edit_link.click()
+
+        self.eventpage.get_event_detail_edit_attendees_text_box.clear()
+        self.eventpage.get_event_detail_edit_attendees_text_box.send_keys("20")
+
+        self.eventpage.get_event_detail_edit_address1_text_box.clear()
+        self.eventpage.get_event_detail_edit_address1_text_box.send_keys("Cancel Details Address 1")
+
+        self.eventpage.get_event_detail_edit_address2_text_box.clear()
+        self.eventpage.get_event_detail_edit_address2_text_box.send_keys("Cancel Details Address 2")
+
+        self.eventpage.get_event_detail_edit_city_text_box.clear()
+        self.eventpage.get_event_detail_edit_city_text_box.send_keys("Cancel Details City")
+
+        self.eventpage.get_event_detail_edit_state_text_box.clear()
+        self.eventpage.get_event_detail_edit_state_text_box.send_keys("Cancel Details State")
+
+        self.eventpage.get_event_detail_edit_zip_text_box.clear()
+        self.eventpage.get_event_detail_edit_zip_text_box.send_keys("Details Zip")
+
+        self.eventpage.get_event_detail_edit_url_text_box.clear()
+        self.eventpage.get_event_detail_edit_url_text_box.send_keys("Cancel http://www.indecomm.net")
+
+        self.eventpage.get_event_detail_edit_description_text_box.clear()
+        self.eventpage.get_event_detail_edit_description_text_box.send_keys("Partner with Indecomm for consulting, outsourcing, technology, and learning solutions")
+        self.eventpage.get_event_detail_edit_cancel_button.click()
+        self.assertEqual(self.eventpage.event_name, self.eventpage.get_event_name_breadcrumb.text)
+
