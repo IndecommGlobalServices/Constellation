d4e6dfb4fcd199f4f57fde258c4406a62a9d5788
Bijesh.Gupta <bijesh.gupta@gmail.com>
2016-06-03 15:52:41 +0530

    Concurrent file has been updated.

--- testcases/concurrent_run.py
+++ testcases/concurrent_run.py
@@ -1,10 +1,8 @@
 from subprocess import Popen
-import glob
-import time, sys
-import os, nose
+import time, os
 from datetime import datetime
 from time import sleep
-import xml.etree.ElementTree as ET
+
 
 start = time.time()
 cwd = os.getcwd()
@@ -38,9 +36,13 @@ assessmenttrainning_filepath = os.path.join(os.getcwd(), r"TestResult-Assessment
                                   str(datetime.today().time().strftime('%H%M'))+".xml")
 login_filepath = os.path.join(os.getcwd(), r"TestResult-Login_" + str(datetime.today().strftime('%Y%m%d')) +"_"+
                                   str(datetime.today().time().strftime('%H%M'))+".xml")
+timeline_filepath = os.path.join(os.getcwd(), r"TestResult-TimeLine_" + str(datetime.today().strftime('%Y%m%d')) +"_"+
+                                  str(datetime.today().time().strftime('%H%M'))+".xml")
+events_filepath = os.path.join(os.getcwd(), r"TestResult-Events_" + str(datetime.today().strftime('%Y%m%d')) +"_"+
+                                  str(datetime.today().time().strftime('%H%M'))+".xml")
+fieldinterviews_filepath = os.path.join(os.getcwd(), r"TestResult-FieldInterviews_" + str(datetime.today().strftime('%Y%m%d')) +"_"+
+                                  str(datetime.today().time().strftime('%H%M'))+".xml")
 os.chdir(cwd)
-
-# assessmenttests = glob.glob('assessment*.py')
 
 tests1 = (['assessmentschoolinfrastructuretest', str(assessmentinfra_filepath), 'AssessmentSchoolInfrastructuretest'],
           ['assettest', str(asset_filepath), 'Assettest'],
@@ -53,17 +55,10 @@ tests1 = (['assessmentschoolinfrastructuretest', str(assessmentinfra_filepath), 
 test2 =  (['maptest', str(map_filepath), 'Maptest'],
           ['assessmentpoliciesandplanningtest', str(assessmentpolicies_filepath), 'AssessmentPoliciesandPlanningtest'],
           ['assessmenttrainingandexercisetest', str(assessmenttrainning_filepath), 'AssessmentTrainingandExercisetest'],
-          ['threatstreamstest', str(threatstream_filepath), 'ThreatStreamtest'])
-#
-# testsordered1 = (['threatstreamstest', str(threatstream_filepath), 'ThreatStreamtest'],
-#                  ['assessmenttest', str(assessment_filepath), 'Assessmenttest'],
-#                  ['assessmentoverviewtest', str(assessmentoverviewtest_filepath), 'AssessmentOverviewtest'],
-#                  ['logintest', str(login_filepath), 'Logintest'],
-#                  ['assessmentschooldatatest', str(assessmentschooldatatest_filepath), 'AssessmentSchoolDatatest'])
-#
-# testsordered2 = (['threatstreamstest', str(threatstream_filepath), 'ThreatStreamtest'],
-#                  ['assessmentoverviewtest', str(assessmentoverviewtest_filepath), 'AssessmentOverviewtest'],
-#                  ['assessmentpoliciesandplanningtest', str(assessmentpolicies_filepath), 'AssessmentPoliciesandPlanningtest'])
+          ['threatstreamstest', str(threatstream_filepath), 'ThreatStreamtest'],
+          ['eventtest', str(events_filepath), 'Eventtest'],
+          ['timelinetest', str(timeline_filepath), 'Timelinetest'],
+          ['fieldinterviewstest', str(fieldinterviews_filepath), 'Fieldinterviewstest'])
 
 processes = []
 
@@ -80,39 +75,5 @@ for test in test2:
 for process in processes:
     process.wait()
 
-# for test in testsordered1:
-#     processes.append(os.system('nosetests --tests ' + test[0] + ' --xunit-file=' + test[1] + ' --xunit-testsuite-name=' + test[2]))
-#     sleep(2)
-#
-# for test in testsordered1:
-#     processes.append(Popen('nosetests --tests ' + test[0] + ' --xunit-file=' + test[1] + ' --xunit-testsuite-name=' + test[2] , shell=True))
-#     sleep(2)
-#
-# for process in processes:
-#     process.wait()
-#
-# for test in testsordered2:
-#     processes.append(Popen('nosetests --tests ' + test[0] + ' --xunit-file=' + test[1] + ' --xunit-testsuite-name=' + test[2] , shell=True))
-#     sleep(2)
-# for test in testsordered2:
-#     processes.append(os.system('nosetests --tests ' + test[0] + ' --xunit-file=' + test[1] + ' --xunit-testsuite-name=' + test[2]))
-#     sleep(2)
-# for process in processes:
-#     process.wait()
-
 print "*" * 50
 print "Time taken: %s minutes" % ((time.time() - start) /60)
-
-# xml_files = glob.glob("..\\"+path+"\\*.xml")
-# xml_element_tree = None
-# for xml_file in xml_files:
-#     print xml_file
-#     # get root
-#     data = ET.parse(xml_file).getroot()
-#     for result in data.iter('testsuite'):
-#         if xml_element_tree is None:
-#             xml_element_tree = result
-#         else:
-#             xml_element_tree.append(result)
-# if xml_element_tree is not None:
-#     ET.ElementTree(xml_element_tree).write(testresult)
